bd6e7adabcbdc692ba2fa99cefece164ce59794b
DeLucaJ <delucaj96@gmail.com>
2024-01-26 13:57:32 -0800

    Updated Gitignore. WIP Meeples

---
+++ src/Interactable/DialogueInteractable.cs
@@ -0,0 +1,7 @@
+﻿namespace YesNoSlap.Interactable;
+
+using Godot;
+
+public partial class DialogueInteractable : Node, IInteractable{
+
+}
---
+++ src/Interactable/IInteractable.cs
@@ -0,0 +1,5 @@
+﻿namespace YesNoSlap.Interactable;
+
+public interface IInteractable {
+
+}
