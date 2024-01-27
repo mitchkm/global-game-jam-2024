499c554cb8157f77c60fe501047a0c47d3bfd362
jthpotz <jthpotz@gmail.com>
2024-01-26 18:01:46 -0800

    Getting used to UI and themes

--- src/Main.cs
+++ src/Main.cs
@@ -18,25 +18,25 @@ public partial class Main : Node2D {
 #endif
 
   public override void _Ready() {
-    #if DEBUG
-    // If this is a debug build, use GoDotTest to examine the
-    // command line arguments and determine if we should run tests.
-    Environment = TestEnvironment.From(OS.GetCmdlineArgs());
-    if (Environment.ShouldRunTests) {
-      CallDeferred("RunTests");
-      return;
-    }
+	#if DEBUG
+	// If this is a debug build, use GoDotTest to examine the
+	// command line arguments and determine if we should run tests.
+	Environment = TestEnvironment.From(OS.GetCmdlineArgs());
+	if (Environment.ShouldRunTests) {
+	  CallDeferred("RunTests");
+	  return;
+	}
 #endif
 
-    // If we don't need to run tests, we can just switch to the game scene.
-    CallDeferred("RunScene");
+	// If we don't need to run tests, we can just switch to the game scene.
+	CallDeferred("RunScene");
   }
 
   #if DEBUG
   private void RunTests()
-    => _ = GoTest.RunTests(Assembly.GetExecutingAssembly(), this, Environment);
+	=> _ = GoTest.RunTests(Assembly.GetExecutingAssembly(), this, Environment);
 #endif
 
   private void RunScene()
-    => GetTree().ChangeSceneToFile("res://scenes/MainMenu.tscn");
+	=> GetTree().ChangeSceneToFile("res://scenes/MainMenu.tscn");
 }
--- src/MainMenu.cs
+++ src/MainMenu.cs
@@ -3,15 +3,17 @@ namespace YesNoSlap;
 using System.Diagnostics;
 using Godot;
 
-public partial class MainMenu : Control {
+public partial class MainMenu : Control
+{
   [Export]
   public string PlayButtonScene = "res://scenes/Game.tscn";
   public Button PlayButton { get; private set; } = default!;
 
   public override void _Ready()
-    => PlayButton = GetNode<Button>("%PlayButton");
+  => PlayButton = GetNode<Button>("%PlayButton");
 
-  public void OnPlayButtonPressed() {
+  public void OnPlayButtonPressed()
+  {
     GetTree().ChangeSceneToFile(PlayButtonScene);
   }
 }
