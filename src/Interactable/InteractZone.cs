a3a99adce9cb1c00e5a6415473422925af0eefdf
DeLucaJ <delucaj96@gmail.com>
2024-01-27 17:27:51 -0800

    interact target added to player

--- src/Interactable/InteractZone.cs
+++ src/Interactable/InteractZone.cs
@@ -4,13 +4,15 @@ namespace YesNoSlap.Interactable;
 
 [GlobalClass]
 public partial class InteractZone : Area3D {
-  [Export] private Interactable _interactTarget;
+  [Export] private Interactable? _interactTarget;
 
   public override void _Ready() => BodyEntered += OnBodyEnteredHandler;
 
   private void OnBodyEnteredHandler(Node3D body) {
-    if (body is PlayerController) {
-      GD.Print("Interact with me!!!");
+    if (body is PlayerController player) {
+      if (_interactTarget != null) {
+        player.InteractTarget = _interactTarget;
+      }
     }
   }
 }
