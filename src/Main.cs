52625db03af81e347b0cab535a38a703adf2a101
Jason H <nusyaj@gmail.com>
2024-01-28 12:29:11 -0800

    Added dialogue for Caline
    Resaved dialogues for Grandma and Slappington to hopefully fix import errors

--- src/Main.cs
+++ src/Main.cs
@@ -38,9 +38,9 @@ public partial class Main : Node2D {
 #endif
 
   private void RunScene() {
-    var MyGameState = GetNode<GameState>("/root/GameState");
-    MyGameState.Load();
-    GetTree().AutoAcceptQuit = false;
-    GetTree().ChangeSceneToFile("res://scenes/MainMenu.tscn");
+	var MyGameState = GetNode<GameState>("/root/GameState");
+	MyGameState.Load();
+	GetTree().AutoAcceptQuit = false;
+	GetTree().ChangeSceneToFile("res://scenes/MainMenu.tscn");
   }
 }
